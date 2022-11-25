e62683b5601d5dd6e4c5ec4d20cc6d732d586933
Khlanji Yassine <4988@cih.ma>
2022-11-25 17:06:19 +0100

    first commit

---
+++ src/main/java/com/javatechie/docker_jenkins_integration/App.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_jenkins_integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
